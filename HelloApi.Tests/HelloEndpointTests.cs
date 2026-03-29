5d5d10efa4dd49f9fe4ab4175a4c870d8fcc4092
Tim <timlearningcicd@outlook.com>
2026-03-29 17:04:01 -0400

    Added a failing test and changed class name

---
+++ HelloApi.Tests/HelloEndpointTests.cs
@@ -0,0 +1,24 @@
+﻿public class HelloEndpointTests
+{
+    [Fact]
+    public void Message_ShouldNotBeEmpty()
+    {
+        var result = new { message = "Hello, World!", timestamp = DateTime.UtcNow };
+        Assert.False(string.IsNullOrEmpty(result.message));
+    }
+
+    [Fact]
+    public void Timestamp_ShouldBeRecentUtc()
+    {
+        var result = new { message = "Hello, World!", timestamp = DateTime.UtcNow };
+        Assert.True(result.timestamp > DateTime.UtcNow.AddMinutes(-1));
+    }
+
+[Fact]
+public void Message_ShouldNotBeEmptyFail()
+{
+    var result = new { message = "Hello, World!", timestamp = DateTime.UtcNow };
+    Assert.Equal("This will never match", result.message);
+}
+
+}
